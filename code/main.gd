af7b6532cc46608cbe57ba7f52b46270b23c4cb9
n_mo <58441304+kotofeyy@users.noreply.github.com>
2026-02-15 19:56:47 +0700

    задник, новая рыба, конфетти на повышение уровня

--- code/main.gd
+++ code/main.gd
@@ -2,9 +2,13 @@ extends Control
 
 @onready var player: Sprite2D = $Player
 @onready var audio_eat: AudioStreamPlayer = $AudioEat
+@onready var audio_level_up: AudioStreamPlayer = $AudioLevelUp
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
+
 @onready var fish_preload = preload("res://scenes/fish.tscn")
-@onready var progress_bar: ProgressBar = $CanvasLayer/Panel/VBoxContainer/ProgressBar
-@onready var level_label: Label = $CanvasLayer/Panel/VBoxContainer/LevelLabel
+@onready var progress_bar: ProgressBar = $CanvasLayer/Panel/MarginContainer/VBoxContainer/ProgressBar
+@onready var level_label: Label = $CanvasLayer/Panel/MarginContainer/VBoxContainer/LevelLabel
+@onready var animated_sprite_eat: AnimatedSprite2D = $AnimatedSpriteEat
 
 
 var has_game_started := false
@@ -13,6 +17,7 @@ var score = 0
 var level = 1
 
 func _ready() -> void:
+	chage_texture(level - 1)
 	level_label.text = "Уровень " + str(level)
 	progress_bar.max_value = level * 10
 	player_scale = Util.size_to_scale[level - 1]
@@ -26,15 +31,18 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if progress_bar.ratio == 1.0:
+		
+		progress_bar.value = 0
+		level += 1
+		cpu_particles_2d.restart()
 		if level < 5:
-			progress_bar.value = 0
-			level += 1
 			player_scale = Util.size_to_scale[level - 1]
 			player.scale = player_scale
-			
-			progress_bar.min_value = score
-			progress_bar.max_value = level * 10
-			level_label.text = "Уровень " + str(level)
+		chage_texture(level - 1)
+		audio_level_up.play()
+		progress_bar.min_value = score
+		progress_bar.max_value = level * 10
+		level_label.text = "Уровень " + str(level)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -47,17 +55,29 @@ func _unhandled_input(event: InputEvent) -> void:
 		player.flip_h = false
 
 
-func on_eat_fish() -> void:
+func on_eat_fish(pos) -> void:
 	audio_eat.play()
+	animated_sprite_eat.global_position = pos
+	animated_sprite_eat.play("eat")
 	score += 1
 	update_progress()
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if level - 1 >= area.get_parent().level:
-		area.get_parent().reset_spawn()
-		on_eat_fish()
+	var parent = area.get_parent()
+	if level - 1 >= parent.level:
+		on_eat_fish(parent.position)
+		parent.reset_spawn()
+		
 	
 
 func update_progress() -> void:
 	progress_bar.value = score
+
+
+func chage_texture(index: int) -> void:
+	var tile_size = 16
+	var x = (index % 3) * tile_size
+	var y = (index / 3) * tile_size
+	
+	player.region_rect = Rect2(x, y, tile_size, tile_size)
--- code/util.gd
+++ code/util.gd
@@ -12,9 +12,9 @@ enum Size {
 
 const size_to_scale = {
 	Size.EXTRA_SMALL : Vector2(2.0, 2.0),
-	Size.SMALL : Vector2(2.3, 2.3),
-	Size.MEDIUM : Vector2(2.6, 2.6),
-	Size.LARGE : Vector2(2.9, 2.9),
-	Size.EXTRA_LARGE : Vector2(3.1, 3.1),
-	Size.DOUBLE_LARGE : Vector2(3.4, 3.4),
+	Size.SMALL : Vector2(2.4, 2.4),
+	Size.MEDIUM : Vector2(2.8, 2.8),
+	Size.LARGE : Vector2(3.2, 3.2),
+	Size.EXTRA_LARGE : Vector2(3.6, 3.6),
+	Size.DOUBLE_LARGE : Vector2(4.0, 4.0),
 }
